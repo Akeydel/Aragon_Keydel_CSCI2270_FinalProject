c9197df5609e18c4ac78f0ed85e9458ac78ec55c
jaar6504 <jackson.aragon@colorado.edu>
2016-04-27 01:33:38 -0600

    Added all files
    All files are 100% complete

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Play.h"
+#include "Create.h"
+using namespace std;
+
+int main(){
+    string userInput;      //Variable used to help user navigate menu;
+    Play Play;
+    Create Create;
+    cout<<"\tHello! Welcome to Dungeon Creator!"<<endl;
+    cout<<"\t**********************************\n"<<endl;
+
+    while(userInput != "3"){                  //This menu will constantly be running. There may be a more optimal way
+        cout<<"===== Main Menu =====\n";
+        cout<<"1. Create a dungeon!\n";
+        cout<<"2. Play a dungeon!\n";
+        cout<<"3. Quit\n"<<endl;
+        cout<<"Which would you like to do? | ";
+        getline(cin, userInput, '\n');
+
+        if(userInput == "1"){
+            Create.createLoop();
+        }
+        else if(userInput == "2"){
+            Play.loadDungeon();
+        }
+        else if(userInput == "3"){
+            cout<<"Thank you for playing!\n";
+            return 0;
+        }
+        else{
+            cout<<"Please make sure to enter a valid option.\n"<<endl;
+        }
+    }
+}
